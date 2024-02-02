cd0aade002743f9d16ad38d907255f92b29df1eb
yangyepope <yangyegong10@163.com>
2024-02-02 11:13:31 +0800

    pure drf

--- pureDRF/urls.py
+++ pureDRF/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home', views.home, name='home'),
+    # path('admin/', admin.site.urls),
+    # path('home', views.home, name='home'),
     path('user/', views.USerView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('order/',views.OrderView.as_view()),
 ]
